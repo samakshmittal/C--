632191549282892b4208c1d7a2832e7e5d737789
samakshmittal <samakshmittal826@gmail.com>
2022-12-30 02:29:25 +0530

    Create thread.cpp

---
+++ thread.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <thread>
+using namespace std;
+void threadfn(){
+    cout<<"Inside thread"<<endl;
+}
+int main(){
+    thread t1(threadfn);
+    t1.join();
+    return 0;
+}
