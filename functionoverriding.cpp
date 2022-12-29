0ee9b0d921c8b1fb1dea9c0fa11fcd1e8be8c388
samakshmittal <samakshmittal826@gmail.com>
2022-12-29 18:51:41 +0530

    Update functionoverriding.cpp

--- functionoverriding.cpp
+++ functionoverriding.cpp
@@ -1,20 +1,12 @@
+// Online C++ compiler to run C++ program online
 #include <iostream>
+#include <thread>
 using namespace std;
-class Mammal {
-    public:
-    void eat() 
-    {
-        cout << "Mammals eat...";
-        }
-        };
-        class Cow: public Mammal {
-            public:
-            void eat() {
-                cout << "Cows eat grass...";
-                }
-                };
-                int main(void) {
-                    Cow c = Cow();
-                    c.eat();
-                    return 0;
-                }
+void threadfn(){
+    cout<<"Inside thread"<<endl;
+}
+int main(){
+    thread t1(threadfn);
+    t1.join();
+    return 0;
+}
